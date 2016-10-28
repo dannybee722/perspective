4761bd89c5d1b3f20b9eb338e456ef3177608b95
hkawoosa <hkawoosa@gmail.com>
2016-10-27 23:46:27 -0400

    shitting around with making things move (plot twist: it doesnt work D':  )

---
+++ Topsy_Turvy/Assets/Scripts/MoveWithSphere.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveWithSphere : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "Sphere")
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                other.rigidbody.AddForce(Vector3.up * 100);
+                this.gameObject.transform.Translate(Vector3.up * Time.deltaTime);
+            }
+        }
+        
+    }
+}
